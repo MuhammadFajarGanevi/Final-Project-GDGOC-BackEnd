054dd6427821da9ec9e9da407d6fca8372b0616a
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-12 16:21:46 +0700

    tambah feature: update, delete, findById

--- src/modules/book/item.service.ts
+++ src/modules/book/item.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -30,8 +30,7 @@ export class ItemService {
   async findAll() {
     const items = await this.itemRepository.find();
 
-    // Hanya ambil item yang sudah dihapus (soft delete)
-    const filteredItems = items.filter((item) => item.deleted_at !== null);
+    const filteredItems = items.filter((item) => item.deleted_at === null);
 
     const result = filteredItems.map((item) => {
       const status =
@@ -45,19 +44,44 @@ export class ItemService {
     return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: number) {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item || item.deleted_at !== null) {
+      throw new NotFoundException(`Buku dengan id ${id} tidak ditemukan`);
+    }
+
+    return item;
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
-    const item = new Item();
-    item.reading = updateItemDto.reading;
-    item.read_page = updateItemDto.read_page;
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item || item.deleted_at !== null) {
+      throw new NotFoundException(`Item dengan id ${id} tidak ditemukan`);
+    }
+
+    // Update field
+    item.reading = updateItemDto.reading ?? item.reading;
+    item.read_page = updateItemDto.read_page ?? item.read_page;
     item.updated_at = new Date();
-    return `This action updates a #${id} item`;
+
+    await this.itemRepository.save(item);
+
+    return item;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item) {
+      throw new NotFoundException('buku dengan id ${id} tidak ditemukan');
+    }
+
+    item.deleted_at = new Date();
+
+    await this.itemRepository.save(item);
+
     return `This action removes a #${id} item`;
   }
 }
