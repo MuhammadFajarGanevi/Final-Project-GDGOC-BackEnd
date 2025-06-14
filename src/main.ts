e1e0f48a1614ae47b68373c9a827164707a05baf
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-14 16:16:25 +0700

    mengecek CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // ✅ Aktifkan CORS
-  app.enableCors({
-    origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
 
   await app.listen(3000);
 }
