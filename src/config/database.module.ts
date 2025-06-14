8c8d4ea5dc1983d1441bbec9f705ecac6e483aa5
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-14 14:55:09 +0700

    Mengubah env

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -5,11 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'backend_gdgoc',
+      host: process.env.MYSQLHOST,
+      port: +process.env.MYSQLPORT,
+      username: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
