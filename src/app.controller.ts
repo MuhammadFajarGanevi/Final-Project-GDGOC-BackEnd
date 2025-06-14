d07fc60b67f9b21649fd798a17badbfd65867d28
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-14 16:21:16 +0700

    mengecek CORS + route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Redirect('/books', 302) // kode 302 = temporary redirect
-  redirectToBooks() {
-    // kamu juga bisa pakai logic di sini jika butuh
-  }
+  // @Get()
+  // @Redirect('/books', 302) // kode 302 = temporary redirect
+  // redirectToBooks() {
+  //   // kamu juga bisa pakai logic di sini jika butuh
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Aktifkan CORS
-  // app.enableCors({
-  //   origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
