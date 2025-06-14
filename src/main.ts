0cee527a76c1bcbbdf43325fda06aeac14f7ea89
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-14 16:39:06 +0700

    Mengaktifkan CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: '*', // atau ganti dengan asal tertentu seperti 'https://example.com'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
