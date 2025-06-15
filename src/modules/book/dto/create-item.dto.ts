158c92041a9ddd848cd775a6b277cc84d40f2830
M Fajar Ganevi <mfajarganevi@gmail.com>
2025-06-15 09:48:02 +0700

    Fix: creatItemDto

--- src/modules/book/dto/create-item.dto.ts
+++ src/modules/book/dto/create-item.dto.ts
@@ -1,8 +1,21 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateItemDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   year: number;
+
+  @IsNotEmpty()
   author: string;
+
+  @IsNotEmpty()
   summary: string;
+
+  @IsNotEmpty()
   publisher: string;
+
+  @IsNotEmpty()
   page_count: number;
 }
